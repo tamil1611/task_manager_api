6daaa35b0688563c8e6fc8aab50de5bac2265640
tamil1611 <tamilselvit@thestackly.com>
2026-07-14 10:24:38 +0530

    Update models.py

---
+++ models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    status = Column(String, default="Pending")
